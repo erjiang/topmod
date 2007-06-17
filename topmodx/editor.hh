3b7259bac175d259f01705f1937253303182ca94
stuart.tett <stuart.tett@40d4e95e-a029-0410-9867-e9b255c29d35>
2007-06-17 19:42:21 +0000

    Adding files for new script editor
    git-svn-id: http://topmod.googlecode.com/svn/trunk@102 40d4e95e-a029-0410-9867-e9b255c29d35

---
+++ topmodx/editor.hh
@@ -0,0 +1,91 @@
+#ifndef _EDITOR_H_
+#define _EDITOR_H_
+
+#include <QTextEdit>
+
+enum ColorType { Number, FunctionName, Variable, MatchedPar };
+
+class QEvent;
+class QKeyEvent;
+class QWheelEvent;
+class QWidget;
+class Evaluator;
+class QStringList;
+
+class Editor : public QTextEdit {
+ Q_OBJECT 
+
+public :
+
+  Editor( QWidget* parent = 0 );
+  ~Editor();
+
+  QSize sizeHint() const;
+
+  QStringList history() const;
+  void setHistory( const QStringList& history );
+
+  bool autoCompleteEnabled() const;
+  void setAutoCompleteEnabled( bool enable );
+
+  void setSyntaxHighlight( bool enable );
+  bool isSyntaxHighlightEnabled() const;
+
+  void setHighlightColor( ColorType type, QColor color );
+  QColor highlightColor( ColorType type );
+
+signals:
+  void ctrlReturnPressed();
+
+public slots:
+  void appendHistory( const QString& text );
+  void clearHistory();
+
+  void moveCursorToEnd();
+
+  void setPlainText(const QString &txt);
+
+protected slots:
+  void checkAutoComplete();
+  void triggerAutoComplete();
+  void autoComplete( const QString& item );
+  void historyBack();
+  void historyForward();
+
+protected:
+  void keyPressEvent( QKeyEvent* );
+  void wheelEvent( QWheelEvent* );
+
+private:
+  class Private;
+  Private* d;
+  Editor( const Editor& );
+  Editor& operator=( const Editor& );
+};
+
+
+class EditorCompletion : public QObject {
+  Q_OBJECT
+
+public:
+  EditorCompletion( Editor* editor );
+  ~EditorCompletion();
+
+  bool eventFilter( QObject *o, QEvent *e );
+  void doneCompletion();
+  void showCompletion( const QStringList &choices );
+
+protected slots:
+  void moveCompletionPopup();
+
+signals:
+  void selectedCompletion( const QString& item );
+
+private:
+  class Private;
+  Private* d;
+  EditorCompletion( const EditorCompletion& );
+  EditorCompletion& operator=( const EditorCompletion& );
+};
+
+#endif // _EDITOR_H_
