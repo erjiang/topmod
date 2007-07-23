ba7ba1725eb3ed2fee0b9418da336d07ebb28d3d
dvmorris <dvmorris@40d4e95e-a029-0410-9867-e9b255c29d35>
2007-07-23 04:14:43 +0000

    bash script to make all the libraries at once
    git-svn-id: http://topmod.googlecode.com/svn/trunk@169 40d4e95e-a029-0410-9867-e9b255c29d35

---
+++ topmodx/makeall.sh
@@ -0,0 +1,13 @@
+# build libraries
+cd include && qmake && make
+
+# python stuff for pydlfl library
+# only works on OS/X and maybe linux for now 
+cd pydlfl && make
+python dlfl_setup.py install
+
+# go back up 2 directory levels
+cd ../../
+
+# build main project
+qmake && make
