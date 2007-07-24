370465cb8868e21b4dd24493bd6328364fa1c105
dvmorris <dvmorris@40d4e95e-a029-0410-9867-e9b255c29d35>
2007-07-24 04:20:28 +0000

    git-svn-id: http://topmod.googlecode.com/svn/trunk@175 40d4e95e-a029-0410-9867-e9b255c29d35

--- topmodx/makeall.sh
+++ topmodx/makeall.sh
@@ -3,7 +3,8 @@ cd include && qmake && make
 
 # python stuff for pydlfl library
 # only works on OS/X and maybe linux for now 
-cd pydlfl && make
+cd pydlfl
+make
 python dlfl_setup.py install
 
 # go back up 2 directory levels
