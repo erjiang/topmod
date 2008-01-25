e5fdf9af36af137e96c9725ff9a5753d223851d3
dvmorris <dvmorris@40d4e95e-a029-0410-9867-e9b255c29d35>
2008-01-25 21:32:35 +0000

    removing old versions
    git-svn-id: http://topmod.googlecode.com/svn/trunk@238 40d4e95e-a029-0410-9867-e9b255c29d35

--- topmod/DLFLRenderer.cc
+++
@@ -1,9 +0,0 @@
-/* $Id: DLFLRenderer.cc,v 4.1 2004/04/18 18:09:06 vinod Exp $ */
-
-// Non-inline function and static variable definitions for DLFLRenderer class
-
-#include "DLFLRenderer.hh"
-
-// int DLFLRenderer::render_flags = 0;
-bool DLFLRenderer::reverse_object = false;
-
--- topmod/include/include.pro
+++
@@ -1,8 +0,0 @@
-TEMPLATE = subdirs
-
-SUBDIRS = \
-	vecmat \
-	arcball \
-	dlflcore \
-	dlflaux
-  
